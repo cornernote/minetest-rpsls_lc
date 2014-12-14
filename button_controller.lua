7e1098dc9692270c7002eda6d1c8a9beca27c891
cornernote <cornernote@gmail.com>
2014-12-14 18:12:33 +1030

    update header comments

--- button_controller.lua
+++ button_controller.lua
@@ -3,7 +3,7 @@
 Rock Paper Scissors Lizard Spock
 LuaController for Minetest
 
-Copyright (c) 2012 cornernote, Brett O'Donnell <cornernote@gmail.com>
+Copyright (c) 2014 cornernote, Brett O'Donnell <cornernote@gmail.com>
 Source Code: https://github.com/cornernote/minetest-rpsls-lc
 License: BSD-3-Clause
 
